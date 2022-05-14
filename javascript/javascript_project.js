c156061d2e6262313a9f43af7e85dd5f35e7b81e
21MaKhA <shomahmuda@gmail.com>
2022-05-14 13:26:08 +0500

    javascript_practice

---
+++ javascript/javascript_project.js
@@ -0,0 +1,134 @@
+
+ /*
+var person = "John Doe"; // data type: string
+var age = 34; // data type: number
+var isMarried = true; //Data type : boolean
+var x =1,
+    y = 2
+var z = x+y;
+console.log(z)
+
+var markWeight = 99;
+var markHeight = 1.63;
+var markBmi = markWeight/(markHeight*markHeight);
+console.log(markBmi);
+
+
+var studentAge = 18;
+var votingAge = 18;
+
+if (studentAge>=votingAge) {
+    console.log("You can vote");
+}
+else{
+    console.log("Come back later");
+}
+
+var studentScore = 87;
+if (studentScore>=90) {
+    console.log("You got A");
+}
+else if (studentScore<90 && studentScore>80) {
+    console.log("You got B");
+}
+else if (studentScore<80 && studentScore>70) {
+    console.log("You got c");
+}else if (studentScore<70 && studentScore>60) {
+    console.log("You got D");
+}
+else{
+    console.log("You got F")
+}
+*/
+//let name1 = "Abdullajon";
+
+//Data type
+// string, number, boolean, null, undefined, object, array
+/*
+const streetName = "President St";
+const houseNumber = 157;
+const tenant = "John Doe";
+console.log(tenant +" lives in "+ streetName + " at house number " + houseNumber);
+
+console.log(`${tenant} lives in ${streetName} at house number ${houseNumber}`);
+
+console.log(tenant.split(' '));
+*/
+
+// Array
+/*
+const cars = ["Audi", "BMW", "Tesla"];
+console.log(cars);
+cars.push("Toyota");
+console.log(cars)
+*/
+
+//Object
+/*
+const person = {
+    name3: "Abdulla",
+    nAge: 14,
+    isStudent: true
+};
+console.log(person.nAge);
+
+const{nAge} = person;
+
+console.log(nAge)
+*/
+
+/*
+const students = [
+    {
+        fName: "John Doe",
+        age: 23,
+        isGraduated: false
+    },
+    {
+        fName: "Slim Shady",
+        age: 44,
+        isGraduated: true
+    },
+    {
+        fName: "Jane Smith",
+        age: 21,
+        isGraduated: false
+    }
+];
+
+//console.log(students[0]);
+for (let i=0; i<students.length; i++) {
+    console.log(i);
+    console.log(students[i].fName);
+}
+*/
+
+/*
+const score = 59;
+if (score>50 && score<90) {
+    console.log("true");
+} else{
+    console.log("false");
+}
+*/
+
+/*
+const score1 = 72;
+const result = score1 > 70 ? "A":"B";
+console.log(result);
+*/
+
+/*
+function greet(time) {
+    switch (time) {
+        case"morning":
+            console.log("Good Morning");
+        break;
+        case "afternoon":
+            console.log("Good afternoon");
+        break;
+    }
+}
+greet("morning")
+*/
+
